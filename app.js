7bddc10a5ec8f9187f99c3e9592b9a8279fbc180
LupanRoman <romalupan89@gmail.com>
2022-03-09 21:11:29 +0200

    First change

---
+++ app.js
@@ -0,0 +1,72 @@
+//onclick = document.getElementById("hours").innerHTML = "4hrs";
+    //hours.style.fontSize = '40px';
+
+
+//Time stamps for daily period
+
+function daily() {
+    let workPresentDaily = document.getElementById("worktime").innerHTML = "4hrs";
+    let workPastDay = document.getElementById("workhours").innerHTML = "Last day - 4hrs";
+
+    let playPresentDay = document.getElementById("playtime").innerHTML = "2hrs";
+    let playPastDay = document.getElementById("playhours").innerHTML = "Last day - 3hrs";
+
+    let studyPresentDay = document.getElementById("studytime").innerHTML = "6hrs";
+    let studyPastDay = document.getElementById("studyhours").innerHTML = "Last day - 5hrs";
+
+    let exercisePresentDay = document.getElementById("exercisetime").innerHTML = "2hrs";
+    let exercisePastDay = document.getElementById("exercisehours").innerHTML = "Last day - 2hrs";
+
+    let socialPresentDay = document.getElementById("socialtime").innerHTML = "1hrs";
+    let socialPastDay = document.getElementById("socialhours").innerHTML = "Last day - 3hrs";
+
+    let selfcarePresentDay = document.getElementById("selfcaretime").innerHTML = "2hrs";
+    let selfcarePastDay = document.getElementById("selfcarehours").innerHTML = "Last day - 4hrs";  
+};
+
+//Time stamps for weekly period
+
+function weekly() {
+    let workPresentDaily = document.getElementById("worktime").innerHTML = "32hrs";
+    let workPastDay = document.getElementById("workhours").innerHTML = "Last week - 36hrs";
+
+    let playPresentDay = document.getElementById("playtime").innerHTML = "10hrs";
+    let playPastDay = document.getElementById("playhours").innerHTML = "Last week - 8hrs";
+
+    let studyPresentDay = document.getElementById("studytime").innerHTML = "4hrs";
+    let studyPastDay = document.getElementById("studyhours").innerHTML = "Last week - 7hrs";
+
+    let exercisePresentDay = document.getElementById("exercisetime").innerHTML = "4hrs";
+    let exercisePastDay = document.getElementById("exercisehours").innerHTML = "Last week - 5hrs";
+
+    let socialPresentDay = document.getElementById("socialtime").innerHTML = "5hrs";
+    let socialPastDay = document.getElementById("socialhours").innerHTML = "Last week - 10hrs";
+
+    let selfcarePresentDay = document.getElementById("selfcaretime").innerHTML = "2hrs";
+    let selfcarePastDay = document.getElementById("selfcarehours").innerHTML = "Last week - 2hrs";
+};
+
+//Monthly time stamp
+
+function monthly() {
+    let workPresentDaily = document.getElementById("worktime").innerHTML = "150hrs";
+    let workPastDay = document.getElementById("workhours").innerHTML = "Last week - 135hrs";
+
+    let playPresentDay = document.getElementById("playtime").innerHTML = "100hrs";
+    let playPastDay = document.getElementById("playhours").innerHTML = "Last week - 110hrs";
+
+    let studyPresentDay = document.getElementById("studytime").innerHTML = "160hrs";
+    let studyPastDay = document.getElementById("studyhours").innerHTML = "Last week - 167hrs";
+
+    let exercisePresentDay = document.getElementById("exercisetime").innerHTML = "170hrs";
+    let exercisePastDay = document.getElementById("exercisehours").innerHTML = "Last week - 160hrs";
+
+    let socialPresentDay = document.getElementById("socialtime").innerHTML = "20hrs";
+    let socialPastDay = document.getElementById("socialhours").innerHTML = "Last week - 30hrs";
+
+    let selfcarePresentDay = document.getElementById("selfcaretime").innerHTML = "50hrs";
+    let selfcarePastDay = document.getElementById("selfcarehours").innerHTML = "Last week - 60hrs";
+};
+
+
+
